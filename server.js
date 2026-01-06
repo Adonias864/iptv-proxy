d6b82537fe040cd74ebb4efc09fa2619a8e1be7a
Adonias864 <119835801+Adonias864@users.noreply.github.com>
2026-01-06 14:58:24 -0400

    Update server.js

--- server.js
+++ server.js
@@ -3,28 +3,26 @@ const fetch = require("node-fetch");
 
 const app = express();
 
-// rota que entrega a lista IPTV
-app.get("/playlist", async (req, res) => {
+app.get("/", (req, res) => {
+  res.send("Proxy IPTV ativo. Use /playlist");
+});
 
-  // LINK IPTV ORIGINAL
+app.get("/playlist", async (req, res) => {
   const IPTV_URL = "http://c.zztech.top/get.php?username=709818988&password=308053394&type=m3u_plus&output=hls";
 
   try {
     const response = await fetch(IPTV_URL);
     const data = await response.text();
 
-    // libera acesso para qualquer site (CORS)
     res.setHeader("Access-Control-Allow-Origin", "*");
     res.setHeader("Content-Type", "application/x-mpegURL");
 
     res.send(data);
-
   } catch (err) {
     res.status(500).send("Erro ao buscar playlist IPTV");
   }
 });
 
-// porta usada pelo Render
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log("Servidor IPTV Proxy rodando");
